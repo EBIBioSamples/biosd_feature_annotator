b49fa5d6b1b50b25078268bdb8db963518a55b7b
Marco Brandizi <brandizi@ebi.ac.uk>
2016-02-04 10:41:42 +0000

    More fixes to EBI scripts.

--- src/main/assembly/resources/cron_purge.sh
+++ src/main/assembly/resources/cron_purge.sh
@@ -4,8 +4,8 @@ mydir=$(pwd)
 cd "$mydir"
 
 # All the stdouts from the LSF processes and other executions
-find ./logs -name '*.out' -not -name 'purge.out' -exec rm -f \{\} \;
+find ./logs/ -name '*.out' -not -name 'purge.out' -exec rm -f \{\} \;
 
 # The oldest logs, let's keep the most recent ones, just in case
-find ./logs -name '*.log' -mtime +30 -exec rm -f \{\} \;
+find ./logs/ -name '*.log' -mtime +30 -exec rm -f \{\} \;
 ./annotate.sh --purge 7 --random-quota 10
