18d62aaf9d04b49f113932a2587ddfc35f8260a4
olgavrou <olgavrou@gmail.com>
2016-05-13 17:14:12 +0100

    Added throttling for OLS client

--- src/main/java/uk/ac/ebi/fg/biosd/annotator/olsclient/utils/HttpRequestHandler.java
+++ src/main/java/uk/ac/ebi/fg/biosd/annotator/olsclient/utils/HttpRequestHandler.java
@@ -1,28 +1,51 @@
 package uk.ac.ebi.fg.biosd.annotator.olsclient.utils;
 
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URI;
+import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Map;
 
 /**
  * Created by olgavrou on 02/12/2015.
  */
 public class HttpRequestHandler {
+
+    public String executeHttpGet(String path, Map<String,String> params) throws IOException, URISyntaxException {
+        //gets a set of parameters, builds uri and executes a GET method
+        //returns the content of the GET method]
+        URIBuilder uriBuilder;
+        URL url = null;
+
+        if(params == null){
+            uriBuilder = new URIBuilder()
+                    .setPath(path);
+        } else {
+            uriBuilder = new URIBuilder()
+                    .setPath(path);
+            for (String key : params.keySet()) {
+                uriBuilder.addParameter(key, params.get(key));
+            }
+        }
+
+        url = uriBuilder.build().toURL();
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod ( "GET" );
+        conn.setRequestProperty ( "Accept", "application/json" );
+        int responseCode = conn.getResponseCode();
+        if (responseCode != 200) {
+                throw new RuntimeException("Failed : HTTP error code : " + responseCode + " on GET: " + url);
+        }
+        return getStringFromInputStream ( conn.getInputStream () );
+
+    }
 
 
     // utility function to convert InputStream to String
@@ -67,121 +90,5 @@ public class HttpRequestHandler {
         }
         return sb.toString();
     }
-
-    public String executeHttpDelete(String path, Map<String,String> params) throws IOException, URISyntaxException {
-        HttpDelete httpDelete;
-        URI uri = null;
-        if(params == null){
-            httpDelete = new HttpDelete(path);
-        } else {
-            URIBuilder uriBuilder = new URIBuilder()
-                    .setPath(path);
-            if (params != null) {
-                for (String key : params.keySet()) {
-                    uriBuilder.addParameter(key, params.get(key));
-                }
-            }
-            uri = uriBuilder.build();
-            httpDelete = new HttpDelete(uri);
-
-        }
-
-        httpDelete.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-        HttpClient client = new DefaultHttpClient();
-
-        HttpResponse response = client.execute(httpDelete);
-        if (response.getStatusLine().getStatusCode() != 200) {
-            throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode() + " on DELETE: " + uri);
-        }
-
-        return getStringFromInputStream(response.getEntity().getContent());
-
-    }
-
-    public String executeHttpGet(String path, Map<String,String> params) throws IOException, URISyntaxException {
-        //gets a set of parameters, builds uri and executes a GET method
-        //returns the content of the GET method]
-        HttpGet httpGet;
-        URI uri = null;
-        if(params == null){
-            httpGet = new HttpGet(path);
-        } else {
-            URIBuilder uriBuilder = new URIBuilder()
-                    .setPath(path);
-            for (String key : params.keySet()) {
-                uriBuilder.addParameter(key, params.get(key));
-            }
-
-            uri = uriBuilder.build();
-            httpGet = new HttpGet(uri);
-
-        }
-
-        httpGet.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-        HttpClient client = HttpClientBuilder.create().build();
-
-        HttpResponse response = client.execute(httpGet);
-        if (response.getStatusLine().getStatusCode() != 200) {
-            throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode() + " on GET: " + uri);
-        }
-
-        return getStringFromInputStream(response.getEntity().getContent());
-
-    }
-
-    //Post with JSON
-    public String executeHttpPost(String path, String json) throws IOException, URISyntaxException {
-        //gets a set of parameters, builds uri and executes a POST method
-        //returns the content of the POST method
-        URI uri = new URIBuilder()
-                .setPath(path)
-                .build(); //http://localhost:8081/archive-web-services/archive/templates
-        HttpPost httpPost = new HttpPost(uri);
-
-        httpPost.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-
-        httpPost.setEntity(new StringEntity(json));
-        HttpClient client = HttpClientBuilder.create().build();
-        HttpResponse response = client.execute(httpPost);
-        if (response.getStatusLine().getStatusCode() != 200) {
-            throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode() + " on POST: " + uri);
-        }
-
-        return getStringFromInputStream(response.getEntity().getContent());
-
-    }
-
-    //Post with passing parameters
-    public String executeHttpPost(String path, Map<String,String> params) throws IOException, URISyntaxException {
-        //gets a set of parameters, builds uri and executes a POST method
-        //returns the content of the POST method]
-        HttpPost httpPost;
-        URI uri = null;
-        if(params == null){
-            httpPost = new HttpPost(path);
-        } else {
-            URIBuilder uriBuilder = new URIBuilder()
-                    .setPath(path); //http://localhost:8081/archive-web-services/archive/templates
-            for (String key : params.keySet()) {
-                uriBuilder.addParameter(key, params.get(key));
-            }
-
-            uri = uriBuilder.build();
-            httpPost = new HttpPost(uri);
-
-        }
-
-        httpPost.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-        HttpClient client = HttpClientBuilder.create().build();
-
-        HttpResponse response = client.execute(httpPost);
-        if (response.getStatusLine().getStatusCode() != 200) {
-            throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode() + " on POST: " + uri);
-        }
-
-        return getStringFromInputStream(response.getEntity().getContent());
-
-    }
-
 
 }
--- src/main/java/uk/ac/ebi/fg/biosd/annotator/olsclient/utils/OLSWebServiceUtils.java
+++ src/main/java/uk/ac/ebi/fg/biosd/annotator/olsclient/utils/OLSWebServiceUtils.java
@@ -1,6 +1,15 @@
 package uk.ac.ebi.fg.biosd.annotator.olsclient.utils;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.Table;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.ac.ebi.bioportal.webservice.model.Ontology;
+import uk.ac.ebi.fg.biosd.annotator.AnnotatorResources;
+import uk.ac.ebi.utils.runcontrol.ChainExecutor;
+import uk.ac.ebi.utils.runcontrol.DynamicRateExecutor;
+import uk.ac.ebi.utils.runcontrol.RateLimitedExecutor;
+import uk.ac.ebi.utils.runcontrol.StatsExecutor;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -12,6 +21,9 @@ import java.util.Map;
  */
 public class OLSWebServiceUtils {
 
+
+    private static Logger log = LoggerFactory.getLogger ( OLSWebServiceUtils.class );
+
     /*
     From an ontologyAcronym it searches OLS for a correct ontology prefix
     Searches first for an exact match, and if none found searches without exact match
@@ -22,20 +34,26 @@ public class OLSWebServiceUtils {
      */
     public String getOntology(String olsLocation, String ontoAcronym){
 
+        // Do you have it in memory?
+        Table<Class, String, Object> store = AnnotatorResources.getInstance ().getStore ();
+        String acronym = (String) store.get(Ontology.class, ontoAcronym);
+        if (acronym != null) return acronym;
 
         Map<String, String> params = new HashMap<>();
         params.put("type", "ontology");
-        String response = searchOLS(olsLocation, ontoAcronym, params, true); // first search for exact match
+        String response = invokeOLS(olsLocation, ontoAcronym, params, true); // first search for exact match
 
         OLSResponseParser olsParser = new OLSResponseParser();
         JsonNode doc = olsParser.getFirstDoc(response);
         if (doc == null){ // not found, now search withought exact
-            response = searchOLS(olsLocation, ontoAcronym, params, false); // first search for exact match
+            response = invokeOLS(olsLocation, ontoAcronym, params, false); // first search for exact match
             doc = olsParser.getFirstDoc(response);
         }
 
         JsonNode ontologyPrefix =  olsParser.getFieldFromDoc(doc, "ontology_prefix");
         if (ontologyPrefix != null){
+            if ( !store.contains ( Ontology.class, acronym ) )
+                store.put ( Ontology.class, ontoAcronym, ontologyPrefix.asText() );
             return ontologyPrefix.asText();
         }
         return null;
@@ -59,7 +77,7 @@ public class OLSWebServiceUtils {
             parameters.put("ontology", ontologyPrefix.toLowerCase());
         }
 
-        response = searchOLS(olsLocation, q, parameters, exact);
+        response = invokeOLS(olsLocation, q, parameters, exact);
 
         return response;
     }
@@ -73,23 +91,97 @@ public class OLSWebServiceUtils {
     @exact: true if you want an exact search
     returns OLS's response
      */
-    public String searchOLS(String olsLocation, String q, Map map, boolean exact){
+    public static String invokeOLS(final String olsLocation, final String q, final Map map, final boolean exact){
 
-        String response = null;
-        Map <String, String> params = new HashMap<>();
-        params.put("q",q);
-        params.put("exact", String.valueOf(exact));
-        params.putAll(map);
-        HttpRequestHandler requestHandler = new HttpRequestHandler();
+        final String[] response = {null};
 
-        try {
-            response = requestHandler.executeHttpGet(olsLocation + "/api/search", params);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        return  response;
+        wrapExecutor.execute ( new Runnable() {
+            @Override
+            public void run () {
+                Map<String, String> params = new HashMap<>();
+                params.put("q", q);
+                params.put("exact", String.valueOf(exact));
+                params.putAll(map);
+                HttpRequestHandler requestHandler = new HttpRequestHandler();
+
+                try {
+                    response[0] = requestHandler.executeHttpGet(olsLocation + "/api/search", params);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        return response[0];
+
     }
+
+    //======================================================================//
+
+    public static final StatsExecutor STATS_WRAPPER = new StatsExecutor ("OLS"); //, Long.parseLong ( System.getProperty ( STATS_SAMPLING_TIME_PROP_NAME, "" + 5 * 60 * 1000 )
+    public static final DynamicRateExecutor RATE_LIMITING_WRAPPER = new OLSRateLimiter ();
+
+    /**
+     * This is used in {@link #invokeOLS(String, String, Map, boolean)}, We have wrap that call with
+     * both a {@link RateLimitedExecutor rate limit wrapper} and a {@link StatsExecutor statistical reporter}.
+     * The former is needed because Bioportal's server doesn't like to be hammered at speeds higher than
+     * 15 calls/sec per process.
+     */
+    private static ChainExecutor wrapExecutor = new ChainExecutor (
+            RATE_LIMITING_WRAPPER,
+            STATS_WRAPPER
+    );
+
+    /**
+     * Our own version, that adapts dynamically to the current performance, using
+     * {@link OLSWebServiceUtils#STATS_WRAPPER}.
+     */
+    protected static class OLSRateLimiter extends DynamicRateExecutor
+    {
+        public final double maxRate;
+
+        public OLSRateLimiter () {
+            this ( Double.MAX_VALUE );
+        }
+
+        public OLSRateLimiter ( double maxRate )
+        {
+            super ( maxRate );
+            this.maxRate = maxRate;
+        }
+
+
+        @Override
+        protected synchronized double setNewRate ()
+        {
+            int totCalls = STATS_WRAPPER.getLastTotalCalls ();
+            if ( totCalls == 0 ) return maxRate;
+
+            double failedCalls = STATS_WRAPPER.getLastFailedCalls () / (double) totCalls;
+            if ( failedCalls <= 0.1 )
+            {
+                if ( Math.abs ( this.getRate () / this.maxRate - 1 ) > 1d/1000 )
+                    // was throttling, going back to normal
+                    log.info ( "OLS back to good performance, throttling ends" );
+                return maxRate;
+            }
+
+            // Degrade the performance gently
+            //
+            double rate =
+                    failedCalls <= 0.30 ? maxRate * 0.8
+                            : failedCalls <= 0.50 ? maxRate * 0.5
+                            : failedCalls <= 0.70 ? maxRate * 0.2
+                            : 0.5;
+
+            if ( Math.abs ( this.getRate () / rate - 1 ) > 1d/1000 )
+                // Wasn't throttling, starting now
+                log.info ( "Throttling OLS to avoid too many fails, calls are slowed down to {} calls/s", rate );
+
+            return rate;
+        } // setNewRate
+    }	// OLSRateLimiter
 
 }
